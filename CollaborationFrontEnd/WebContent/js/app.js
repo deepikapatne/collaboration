758e7c3593545a326fa849aa609785ef1864ee98
deepikapatne <deeps.patne@gmail.com>
2017-03-25 15:58:49 +0700

    Add files via upload
    js

--- CollaborationFrontEnd/WebContent/js/app.js
+++ CollaborationFrontEnd/WebContent/js/app.js
@@ -7,7 +7,7 @@ app.config(function($routeProvider)
 	.when('/home',
 		{
 			templateUrl : 'views/home.html',
-			//controller : 'HomeController'
+			controller : 'FriendController'
 		})
 	.when('/login',
 	{
@@ -27,73 +27,155 @@ app.config(function($routeProvider)
 		.when('/viewUsers',
 	{
 		templateUrl : 'C_user/userList.html',
-		controller : 'UserController'	
+		controller : 'UserListController'	
 	})
 		.when('/viewBlogs',
 	{
 		templateUrl : 'C_blog/blogs.html',
 		controller : 'BlogController'	
 	})
+	   .when('/viewBlog',
+			{
+		templateUrl : 'C_blog/viewBlog.html',
+//		controller : 'BlogController'	
+	})
 		.when('/viewJobs',
 	{
 		templateUrl : 'C_job/jobs.html',
 		controller : 'JobController'	
 	})
-	
+	.when('/viewEvents',
+			{
+				templateUrl : 'C_event/viewEvents.html',
+				controller : 'EventController'	
+			})
+	.when('/Chat',
+			{
+				templateUrl : 'C_chat/chat.html',
+				controller : 'ChatController'	
+			})
+		.when('/upload',
+			{
+				templateUrl : 'C_user/fileUpload.html',
+//				controller : 'ChatController'	
+			})
+		.when('/admin',
+			{
+				templateUrl : 'C_admin/adminHome.html',	
+			})	
+		.when('/manageBlogs',
+			{
+				templateUrl : 'C_admin/manageBlogs.html',
+				controller : 'AdminController'
+			})
+		.when('/manageEvents',
+			{
+				templateUrl : 'C_admin/manageEvents.html',	
+				controller : 'AdminController'
+			})
+			.when('/addEvents',
+			{
+				templateUrl : 'C_admin/manageEvents.html',	
+//				controller : 'AdminController'
+			})
+		.when('/manageForums',
+			{
+				templateUrl : 'C_admin/manageForums.html',
+				controller : 'AdminController'
+			})
+		.when('/manageJobs',
+			{
+				templateUrl : 'C_admin/manageJobs.html',
+				controller : 'AdminController'
+			})
+		.when('/addJobs',
+			{
+				templateUrl : 'C_admin/manageJobs.html',	
+//				controller : 'AdminController'
+			})
+		.when('/manageUsers',
+			{
+				templateUrl : 'C_admin/manageUsers.html',	
+				controller : 'AdminController'
+			})
+		.when('/myProfile',
+			{
+				templateUrl : 'C_user/myProfile.html',	
+				controller : 'FriendController'
+			})		
+		.when('/myFriends',
+			{
+				templateUrl : 'C_user/myProfile.html',	
+				controller : 'FriendController'
+			})	
+		.when('/pendingRequests',
+			{
+				templateUrl : 'C_user/myProfile.html',	
+				controller : 'FriendController'
+			})
+		.when('/sentRequests',
+			{
+				templateUrl : 'C_user/myProfile.html',	
+				controller : 'FriendController'
+			})
+		.when('/cmred',
+			{
+				templateUrl : 'C_blog/comment-redirect.html',
+			})	
 	.otherwise({redirectTo: '/'});
 });
 
-app.run( function ($rootScope, $location, $cookieStore, $http) {
 
-	 $rootScope.$on('$locationChangeStart', function (event, next, current) {
-		 console.log("$locationChangeStart")
-		
-		 var userPages = ['/viewUsers']
-		 var adminPages = ["/viewBlogs","/viewJobs"]
-		 
-		 var currentPage = $location.path()
-		 
-		 var isUserPage = $.inArray(currentPage, userPages) ==1;
-		 var isAdminPage = $.inArray(currentPage, adminPages) ==1;
-		 
-		 var isLoggedIn = $rootScope.currentUser.username;
-	        
-	     console.log("isLoggedIn:" +isLoggedIn)
-	     console.log("isUserPage:" +isUserPage)
-	     console.log("isAdminPage:" +isAdminPage)
-	        
-	        if(!isLoggedIn)
-	        	{
-	        	
-	        	 if (isUserPage || isAdminPage) {
-		        	  console.log("Navigating to login page:")
-		        	  alert("You need to loggin to do this operation")
 
-						            $location.path('/');
-		                }
-	        	}
-	        
-			 else //logged in
-	        	{
-	        	
-				 var role = $rootScope.currentUser.role;
+app.run( function ($rootScope, $location, $cookieStore, $http) 
+		{
+			 $rootScope.$on('$locationChangeStart', function (event, next, current) 
+			 {
+				 console.log("$locationChangeStart")
+				    
+				 var userPages = ['/myProfile','myFriends','pendingRequests','sentRequests','/upload','/viewUsers'];
+		         var adminPages = ['/admin','/manageUsers','/manageJobs','/manageEvents','/manageForums','/manageBlogs','/addEvents','/addJobs'];
+		 
+				 var currentPage = $location.path();
 				 
-				 if(isAdminPage && role!='ADMIN' )
-					 {
+				 var isUserPage = $.inArray(currentPage, userPages);
+				 var isAdminPage = $.inArray(currentPage, adminPages);
 					 
-					  alert("You can not do this operation as you are logged as : " + role )
-					   $location.path('/login');
-					 
-					 }
-	        	}
-	 }
-	       );
-	 
-	 
-	 // keep user logged in after page refresh
-    $rootScope.currentUser = $cookieStore.get('currentUser') || {};
-    if ($rootScope.currentUser) {
-        $http.defaults.headers.common['Authorization'] = 'Basic' + $rootScope.currentUser; 
-    }
-
-});
+				 
+				 var isLoggedIn = $rootScope.currentUser.username;
+			        
+			     console.log("isLoggedIn:" +isLoggedIn)
+			     console.log("isUserPage:" +isUserPage)
+			     console.log("isAdminPage:" +isAdminPage)
+			        
+			        if(!isLoggedIn)
+			        	{
+			        	
+			        		if(isUserPage!=-1 || isAdminPage!=-1)  
+			        	 	{
+				        	  console.log("Navigating to login page:")
+				        	  alert("You need to Login first!")
+				        	  $location.path('/login');
+				         	}
+			        	}
+			        
+					 else //logged in
+			        	{
+			        	
+						 var role = $rootScope.currentUser.role;
+						 if(isAdminPage!=-1 && role!='ADMIN' )
+							 {
+							  alert("You cannot view this page as a " + role )
+							  $location.path('/');
+							 
+							 }
+			        	}
+			 });
+			 
+			 // keep user logged in after page refresh
+		    $rootScope.currentUser = $cookieStore.get('currentUser') || {};
+		    if ($rootScope.currentUser)
+		    {
+		        $http.defaults.headers.common['Authorization'] = 'Basic' + $rootScope.currentUser; 
+		    }
+		});
